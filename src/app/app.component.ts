410e567fc9b6619d8959df86d70b3bd2e8dd2c08
ElMohMoh <chaiebm8@gmail.com>
2017-09-19 22:13:02 +0200

    chat pock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent  implements OnInit{
+
+  @ViewChild('myvideo') myVideo: any;
+
+  peer;
+  anotherId;
+  myPeerId;
+
+  consturctor(){
+
+  }
+
+
+  ngOnInit(){
+    let video = this.myVideo.nativeElement;
+    this.peer = new Peer({key: 'wsomd7kknmmzehfr'});
+    setTimeout(()=>{
+      this.myPeerId = this.peer.id;
+
+    },3000)
+    console.log(this.peer);
+    this.peer.on('connection', function(conn) {
+      conn.on('data', function(data){
+        // Will print 'hi!'
+        console.log(data);
+      });
+    });
+
+    var n = <any>navigator;
+    n. getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+    this.peer.on('call', function(call){
+      n.getUserMedia({video: true, audio: true}, function(stream){
+        call.answer(stream);
+        call.on('stream',function(remotestream){
+          video.src = URL.createObjectURL(remotestream);
+          video.play();
+        });
+      },function(err){
+        console.log('Failed to get stream', err);
+      });
+    });
+  }
+
+  connect(){   
+    var conn = this.peer.connect(this.anotherId);
+      conn.on('open', function(){
+        conn.send('Message from that id');
+      });
+    }
+    
+  videoconnect(){
+    let video = this.myVideo.nativeElement;
+    var localvar = this.peer;
+    var fname = this.anotherId;
+
+    //var getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+    var n = <any>navigator;
+    n. getUserMedia = n.getUserMedia || n.webkitGetUserMedia || n.mozGetUserMedia;
+
+    n.getUserMedia({video: true, audio: true}, function(stream){
+      var call = localvar.call(fname, stream);
+      call.on('stream',function(remotestream){
+        video.src = URL.createObjectURL(remotestream);
+        video.play();
+      });
+    },function(err){
+      console.log('Failed to get stream', err);
+    });
+  }
+
 }
